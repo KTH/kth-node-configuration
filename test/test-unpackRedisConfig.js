104990562feb0f5027c16f14aedba36388b5884e
jhsware <sebastian@urbantalk.se>
2017-01-18 13:00:04 +0100

    Fixing Redis config for Azure

--- test/test-unpackRedisConfig.js
+++ test/test-unpackRedisConfig.js
@@ -3,15 +3,14 @@
 const expect = require('chai').expect
 const unpackRedisConfig = require('../lib/unpackRedisConfig')
 
-const testURI = 'redis://localhost:6379/?auth=server.se&tsl[server]=tsl.server.se'
+const testURI = 'redis://localhost:6379/'
+const testURIWithSSL = 'redis://localhost:6379/?auth_pass=abcdefghij&ssl=true'
 
 describe('unpackRedisConfig', function () {
   it('can decode a Redis URI from fallback URI', function () {
     const obj = unpackRedisConfig('no-env-exists', testURI)
     expect(obj.host).to.equal('localhost')
     expect(obj.port).to.equal(6379)
-    expect(obj.auth).to.equal('server.se')
-    expect(obj.tsl.server).to.equal('tsl.server.se')
   })
 
   it('can decode a Redis URI from env var', function () {
@@ -19,21 +18,23 @@ describe('unpackRedisConfig', function () {
     const obj = unpackRedisConfig('TEST_ENV_NOW_HERE')
     expect(obj.host).to.equal('localhost')
     expect(obj.port).to.equal(6379)
-    expect(obj.auth).to.equal('server.se')
-    expect(obj.tsl.server).to.equal('tsl.server.se')
   })
 
   it('can decode a Redis URI from fallback URI and merge with options', function () {
     const obj = unpackRedisConfig('no-env-exists', testURI, { extraOption: true })
     expect(obj.host).to.equal('localhost')
     expect(obj.port).to.equal(6379)
-    expect(obj.auth).to.equal('server.se')
-    expect(obj.tsl.server).to.equal('tsl.server.se')
     expect(obj.extraOption).to.equal(true)
   })
 
   it('should not expose protocol property', function () {
     const obj = unpackRedisConfig('no-env-exists', testURI)
     expect(obj.protocol).to.equal(undefined)
+  })
+
+  it('can handle ssl for Azure', function () {
+    const obj = unpackRedisConfig('no-env-exists', testURIWithSSL)
+    expect(obj.auth_pass).to.equal('abcdefghij')
+    expect(obj.tls.servername).to.equal('localhost')
   })
 })
