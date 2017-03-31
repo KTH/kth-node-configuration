575fe1c8f187118cb53bafbee73bc6340125a0f4
jhsware <sebastian@urbantalk.se>
2017-03-31 11:15:34 +0200

    Refactored how we define multiple API_KEYS

--- test/test-unpackApiKeysConfig.js
+++ test/test-unpackApiKeysConfig.js
@@ -3,22 +3,45 @@
 const expect = require('chai').expect
 const unpackApiKeysConfig = require('../lib/unpackApiKeysConfig')
 
-const testURISingle = '?name=devClient&apiKey=1234&scope=write&scope=read'
-const testURIDouble = '?name=devClient&apiKey=1234&scope=write&scope=read ?name=other&apiKey=5678&scope=read'
+const uriOne = '?name=devClient&apiKey=1234&scope=write&scope=read'
+const uriTwo = '?name=other&apiKey=5678&scope=read'
 
 describe('unpackApiKeysConfig', function () {
   it('can decode a single API key', function () {
-    const obj = unpackApiKeysConfig('no-env-exists', testURISingle)
+    process.env.API_KEY = uriOne
+
+    const obj = unpackApiKeysConfig('API_KEY', undefined)
     expect(obj[0].name).to.equal('devClient')
     expect(obj[0].apiKey).to.equal('1234')
     expect(obj[0].scope[0]).to.equal('write')
     expect(obj[0].scope.length).to.equal(2)
+
+    process.env.API_KEY = undefined
   })
 
-  it('can decode two API keys', function () {
-    const obj = unpackApiKeysConfig('no-env-exists', testURIDouble)
+  it('can decode two API key', function () {
+    process.env['API_KEY[0]'] = uriOne
+    process.env['API_KEY[1]'] = uriTwo
+
+    const obj = unpackApiKeysConfig('API_KEY', undefined)
+    
+    expect(obj[0].name).to.equal('devClient')
+    expect(obj[0].apiKey).to.equal('1234')
+    expect(obj[0].scope[0]).to.equal('write')
+    expect(obj[0].scope.length).to.equal(2)
     expect(obj[1].name).to.equal('other')
     expect(obj[1].apiKey).to.equal('5678')
     expect(obj[1].scope[0]).to.equal('read')
+
+    process.env['API_KEY[0]'] = undefined
+    process.env['API_KEY[1]'] = undefined
+  })
+
+  it('can fall back to default', function () {
+    const obj = unpackApiKeysConfig('no-env-exists', uriOne)
+    expect(obj[0].name).to.equal('devClient')
+    expect(obj[0].apiKey).to.equal('1234')
+    expect(obj[0].scope[0]).to.equal('write')
+    expect(obj[0].scope.length).to.equal(2)
   })
 })
